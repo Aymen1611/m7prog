16f003441cf43b85d2f69a0fd2f98a6b9f9125dd
aymen <35670@ma-web.nl>
2024-04-16 11:03:45 +0100

    les 5.2

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 Route::get('/about', [ \App\Http\Controllers\AboutController::class, 'aboutMe'])->name('about.me');
+
 
 require __DIR__.'/auth.php';
 
